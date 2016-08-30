8f584e24027d61fb0bafc32dbd0f39ab1098674f
tuoitrevohoc <tuoitrevohoc@me.com>
2016-08-30 13:45:09 +0700

    + add build settings for visual studio code.

--- src/NetExpress/Controllers/UserController.cs
+++ src/NetExpress/Controllers/UserController.cs
@@ -1,9 +1,5 @@
 ﻿using NetExpress.Core.Controllers;
 using NetExpress.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using NetExpress.Core.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
